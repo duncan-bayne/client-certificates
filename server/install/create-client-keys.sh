32fa93340146ea26cd0284c3c183e17706965538
Duncan Bayne <duncan.bayne@rea-group.com>
2013-07-24 22:25:57 +1000

    Initial cut at installer.

---
+++ server/install/create-client-keys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+openssl genrsa -des3 -out client.key 4096
+openssl req -new -key client.key -out client.csr
+openssl x509 -req -days 365 -in client.csr -CA server.crt -CAkey server.key -set_serial 01 -out client.crt
+openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.p12
+openssl pkcs12 -in client.p12 -out client.pem -clcerts
