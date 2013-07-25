4ff8d64e06817d6938f0b5441cad19fb16940635
Duncan Bayne <duncan.bayne@rea-group.com>
2013-07-25 17:53:37 +1000

    Now generates passphrase-less client keys.  How the hell did this ever work?

--- server/install/create-client-keys.sh
+++ server/install/create-client-keys.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-openssl genrsa -des3 -out client.key 4096
+openssl genrsa -out client.key 4096
 openssl req -new -key client.key -out client.csr
 openssl x509 -req -days 365 -in client.csr -CA server.crt -CAkey server.key -set_serial 01 -out client.crt
 openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.p12
